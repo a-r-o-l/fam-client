41977114a4768da327b65fdc190734b66c4fabf3
arol <arol@blueverse.club>
2024-04-24 03:41:11 -0300

    fix http url

--- src/services/http.js
+++ src/services/http.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const http = axios.create({
-  baseURL: "fam-api-production.up.railway.app",
+  baseURL: "https://fam-api-production.up.railway.app",
   headers: {
     "Content-Type": "application/json",
   },
